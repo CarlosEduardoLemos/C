71e6e1259108cd11182014b335ab6dfd3538ff71
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2022-01-10 11:44:49 -0300

    Atualizado

--- Códigos e Desafios/Códigos/ex010.c
+++ Códigos e Desafios/Códigos/ex010.c
@@ -9,11 +9,13 @@ int main(){
     if (num2 == 0){
         printf("Divisão por 0 não permitido.\n");
     }else{
+        float result = num1/num2;
         if (num1 % num2 == 0){
             printf("%i é divisivel por %i.\n", num1, num2);
-            printf("%i / %i = %f\n", num1, num2, num1/num2);
+            printf("%i / %i = %f\n", num1, num2, result);
         }else{
             printf("%i não é divisivel por %i.\n", num1, num2);
+            printf("%i / %i = %f\n", num1, num2, result);
         }
         
     }
