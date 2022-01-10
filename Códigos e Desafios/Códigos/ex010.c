402a71e4241514c6c937ce1e8977f344d095baae
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2022-01-10 11:02:15 -0300

    Atualizado

---
+++ Códigos e Desafios/Códigos/ex010.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+
+    printf("Digite 2 números inteiros: \n");
+    scanf("%i %i", &num1, &num2);
+
+    if (num2 == 0){
+        printf("Divisão por 0 não permitido.\n");
+    }else{
+        if (num1 % num2 == 0){
+            printf("%i é divisivel por %i.\n", num1, num2);
+            printf("%i / %i = %f\n", num1, num2, num1/num2);
+        }else{
+            printf("%i não é divisivel por %i.\n", num1, num2);
+        }
+        
+    }
+
+    return 0;   
+}
