067d0902fff064a9e0a071182fc926fb1a133d38
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2022-01-18 20:12:25 -0300

    Atualizado

---
+++ Códigos e Desafios/Códigos/ex025.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+    int matriz[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    void funcaoPrint(int x[3][3]);
+    funcaoPrint(matriz);
+    system("pause");
+    return 0;
+}
+
+void funcaoPrint(int x[3][3]){
+    int i, j;
+    for (i = 0; i < 3; ++i){
+        for (j = 0; j < 3; ++j){
+            printf("%i", x[i][j]);
+        }
+        printf("\n");
+    }
+}
